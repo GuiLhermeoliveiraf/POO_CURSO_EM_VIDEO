2e8c422e915caf5d39f5612cacf960c03d345029
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-10-15 22:12:10 -0300

    Delete Atividade directory

--- Atividade/Atividade3/Principal.java
+++
@@ -1,9 +0,0 @@
-public class Principal {
-    public static void main(String[] args){
-        Caneta c1 = new Caneta("Bic", "Azul", 0.5f);
-        c1.status();
-    
-        Caneta c2 = new Caneta("Abs", "Preta", 0.9f);
-        c2.status();
-    }
-}
