473541907f16c1d37c34978d0e1c499b8cb15182
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-10-18 08:28:37 -0300

    Create Interface.java

---
+++ Exercícios/Aula9_exercícios/Interface.java
@@ -0,0 +1,12 @@
+// Lista de metados para serem importados
+public interface Interface {
+    public abstract void abrir();
+
+    public abstract void fechar();
+
+    public abstract void folhear();
+
+    public abstract void avancarPag(int num);
+
+    public abstract void voltarPag(int num);
+}
