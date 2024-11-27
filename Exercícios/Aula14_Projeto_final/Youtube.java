6411e50f3fb6ef1bd1fe76bfba4790b0c51f7511
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-11-27 08:14:58 -0300

    Create Youtube.java

---
+++ Exercícios/Aula14_Projeto_final/Youtube.java
@@ -0,0 +1,13 @@
+public class Youtube {
+    public static void main(String[] args) {
+        Video v[] = new Video[3];
+        v[0] = new Video("Deu a louca");
+        v[1] = new Video("Testee");
+        v[2] = new Video("Náo");
+
+        Gafanhoto g[] = new Gafanhoto[2];
+
+        g[0] = new Gafanhoto("Guilherme", 20, "Masculino", "Gmail");
+        g[1] = new Gafanhoto("Daniel", 50, "Masculino", "Fapam");
+    }
+}
