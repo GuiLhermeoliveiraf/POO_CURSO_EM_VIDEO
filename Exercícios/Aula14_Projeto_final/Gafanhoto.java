2889075aa049c6a7c09993c79fb9facb6687900b
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-11-27 08:14:42 -0300

    Create Gafanhoto.java

---
+++ Exercícios/Aula14_Projeto_final/Gafanhoto.java
@@ -0,0 +1,32 @@
+public class Gafanhoto extends Pessoa {
+    private String login;
+    private int totAssistindo;
+
+    public Gafanhoto(String nome, int idade, String sexo, String login) {
+        super(nome, idade, sexo);
+        setLogin(login);
+        setTotAssistindo(0);
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getTotAssistindo() {
+        return totAssistindo;
+    }
+
+    public void setTotAssistindo(int totAssistindo) {
+        this.totAssistindo = totAssistindo;
+    }
+
+    @Override
+    public String toString() {
+        return "Gafanhoto " + super.toString() + "[login=" + login + ", totAssistindo=" + totAssistindo + "]";
+    }
+
+}
