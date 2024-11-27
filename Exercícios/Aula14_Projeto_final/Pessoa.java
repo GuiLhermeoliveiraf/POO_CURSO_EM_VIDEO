f39d81a1917059686bd52dcfe74d2e25f904236d
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-11-27 08:14:19 -0300

    Create Pessoa.java

---
+++ Exercícios/Aula14_Projeto_final/Pessoa.java
@@ -0,0 +1,55 @@
+public abstract class Pessoa {
+    protected String nome;
+    protected int idade;
+    protected String sexo;
+    protected float experiencia;
+
+    public Pessoa(String nome, int idade, String sexo) {
+        setExperiencia(0);
+        setIdade(idade);
+        setNome(nome);
+        setSexo(sexo);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public float getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(float experiencia) {
+        this.experiencia = experiencia;
+    }
+
+    public void ganharExperiencia() {
+        setExperiencia(experiencia++);
+    }
+
+    @Override
+    public String toString() {
+        return "Nome= " + nome + ", idade= " + idade + ", sexo= " + sexo + ", experiencia= " + experiencia;
+    }
+
+}
